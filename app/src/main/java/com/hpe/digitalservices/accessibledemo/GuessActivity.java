7726a9a525e459a4a11c83ccf453e78757b619e5
Dave Truxall <david.truxall@hp.com>
2016-09-27 17:22:44 -0400

    Added toast to guess page

--- app/src/main/java/com/hpe/digitalservices/accessibledemo/GuessActivity.java
+++ app/src/main/java/com/hpe/digitalservices/accessibledemo/GuessActivity.java
@@ -1,12 +1,14 @@
 package com.hpe.digitalservices.accessibledemo;
 
+import android.icu.text.DateFormat;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class GuessActivity extends BaseActivity {
-
-    private ArrayAdapter<CharSequence> reasonList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +19,31 @@ public class GuessActivity extends BaseActivity {
 
         final Spinner spinner = (Spinner) findViewById(R.id.reasons);
 
-        reasonList = ArrayAdapter.createFromResource(this, R.array.reasons, android.R.layout.simple_spinner_item);
+        final ArrayAdapter<CharSequence> reasonList = ArrayAdapter.createFromResource(this, R.array.reasons, android.R.layout.simple_spinner_item);
         reasonList.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinner.setAdapter(reasonList);
+
+        Button guessButton = (Button) findViewById(R.id.submit);
+
+        guessButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                long currentEpoch = System.currentTimeMillis();
+
+                CharSequence message;
+
+                if(currentEpoch % 2 == 0) {
+                    message = getResources().getString(R.string.guess_correct);
+                } else {
+                    message = getResources().getString(R.string.guess_wrong);
+                }
+
+                Toast result = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG);
+                result.show();
+            }
+        });
+
     }
 }
